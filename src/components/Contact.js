fc4cfe9ff8957bd28bd4942c17a8c069fd7d473f
adam <atomicpenguines@gmail.com>
2018-10-27 21:32:49 +0100

    added toggle button to show contact information based on state

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,9 @@ import PropTypes from "prop-types";
 import "./Contact.css";
 
 export default class Contact extends Component {
-  state = {};
+  state = {
+    showContactInfo: true
+  };
 
   // same as the type checking at the end of this file
   static propTypes = {
@@ -11,22 +13,28 @@ export default class Contact extends Component {
   };
 
   onShowClick = e => {
-    console.log(this.state);
+    this.setState({
+      showContactInfo: !this.state.showContactInfo
+    });
   };
 
   render() {
     // destructuring to pass values
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
 
     return (
       <div className="card card-body mb-3">
         <h4>
           {name} <i onClick={this.onShowClick} className="fas fa-sort-down" />
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">{email}</li>
-          <li className="list-group-item">{phone}</li>
-        </ul>
+
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">{email}</li>
+            <li className="list-group-item">{phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }

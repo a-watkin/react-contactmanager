bc549929d76ef81bf31dfdf2cc50881fb12d83fe
adam <atomicpenguines@gmail.com>
2018-10-27 17:21:48 +0100

    type checking

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export default class Contact extends Component {
+  // same as the type checking at the end of this file
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired
+  };
+
   render() {
     // destructuring to pass values
     const { name, email, phone } = this.props;
@@ -16,3 +24,9 @@ export default class Contact extends Component {
     );
   }
 }
+
+// Contact.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   email: PropTypes.string.isRequired,
+//   phone: PropTypes.string.isRequired
+// };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Header = props => {
   return (
@@ -6,6 +7,16 @@ const Header = props => {
       <h1>{props.branding}</h1>
     </div>
   );
+};
+
+// displays if there is no prop, an empty string doesn't count as no prop
+Header.defaultProps = {
+  branding: "Default title"
+};
+
+// type checking, will display warning in console if type is incorrect
+Header.propTypes = {
+  branding: PropTypes.string.isRequired
 };
 
 export default Header;

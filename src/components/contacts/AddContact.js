600c405e4083f1420d561a0d90f6d0f69a8fa578
adam <atomicpenguines@gmail.com>
2018-10-28 18:02:49 +0000

    added form for submitting new cointact, this just consoles out the value for now

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -7,6 +7,15 @@ export default class AddContact extends Component {
     phone: "454354343543"
   };
 
+  onSubmit = e => {
+    //   prevents default submitting of the form
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  // the brackets get the value of the name attribute of whatever calls onChange
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
   render() {
     const { name, email, phone } = this.state;
 
@@ -14,41 +23,46 @@ export default class AddContact extends Component {
       <div className="card mb-3">
         <div className="card-header">Add Contact</div>
         <div className="card-body">
-          <form action="" className="form-group">
-            <label htmlFor="name">Name</label>
+          <form className="form-group" onSubmit={this.onSubmit}>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                name="name"
+                className="form-control form-control-lg"
+                placeholder="Enter name..."
+                value={name}
+                onChange={this.onChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="name">Email</label>
+              <input
+                type="email"
+                name="email"
+                className="form-control form-control-lg"
+                placeholder="Enter email address..."
+                value={email}
+                onChange={this.onChange}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">Phone</label>
+              <input
+                type="text"
+                name="phone"
+                className="form-control form-control-lg"
+                placeholder="Enter phone number..."
+                value={phone}
+                onChange={this.onChange}
+              />
+            </div>
             <input
-              type="text"
-              name="name"
-              className="form-control form-control-lg"
-              placeholder="Enter name..."
-              value={name}
+              type="submit"
+              value="Add Contact"
+              className="btn btn-light btn-block"
             />
           </form>
-          <form action="" className="form-group">
-            <label htmlFor="name">Email</label>
-            <input
-              type="email"
-              name="email"
-              className="form-control form-control-lg"
-              placeholder="Enter email address..."
-              value={email}
-            />
-          </form>
-          <form action="" className="form-group">
-            <label htmlFor="phone">Phone</label>
-            <input
-              type="text"
-              name="phone"
-              className="form-control form-control-lg"
-              placeholder="Enter phone number..."
-              value={phone}
-            />
-          </form>
-          <input
-            type="submit"
-            value="Add Contact"
-            className="btn btn-light btn-block"
-          />
         </div>
       </div>
     );

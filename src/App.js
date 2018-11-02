db94f4ea5788dc3ee54582c2a930058fea99e3aa
adam <atomicpenguines@gmail.com>
2018-11-02 05:07:34 +0000

    changed to HashRouter for github pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 // browser router stores the route components
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Contacts from "./components/contacts/Contacts";
 import "bootstrap/dist/css/bootstrap.min.css";

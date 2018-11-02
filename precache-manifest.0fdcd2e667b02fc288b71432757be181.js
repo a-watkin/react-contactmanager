58a51a887c826931dcd235e2f527b43e91971adb
adam <atomicpenguines@gmail.com>
2018-11-02 05:05:33 +0000

    Updates

---
+++ precache-manifest.0fdcd2e667b02fc288b71432757be181.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "713dc91d7c5875520970",
+    "url": "/react-contactmanager/static/css/main.88961237.chunk.css"
+  },
+  {
+    "revision": "713dc91d7c5875520970",
+    "url": "/react-contactmanager/static/js/main.713dc91d.chunk.js"
+  },
+  {
+    "revision": "ea5ddfe637efe0feb218",
+    "url": "/react-contactmanager/static/css/1.b948db40.chunk.css"
+  },
+  {
+    "revision": "ea5ddfe637efe0feb218",
+    "url": "/react-contactmanager/static/js/1.ea5ddfe6.chunk.js"
+  },
+  {
+    "revision": "3e40d23b8b61876c686f",
+    "url": "/react-contactmanager/static/js/runtime~main.3e40d23b.js"
+  },
+  {
+    "revision": "f13d716f61059988b13e3264a1e7e491",
+    "url": "/react-contactmanager/index.html"
+  }
+];
